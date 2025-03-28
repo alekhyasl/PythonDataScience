f14d95eee928ae09b9474f08eb5705fe4e80b7c4
alekhyamsl <alekhya.mogalapu1994@gmail.com>
2025-03-27 22:54:14 -0500

    committing python code

---
+++ PythonBasics2/package/__init__.py
@@ -0,0 +1,3 @@
+"""
+this is a special file which is used in python to define packages and initialise their namespace
+"""
---
+++ PythonBasics2/test.py
@@ -0,0 +1,7 @@
+from package.maths import * # to import all functions from maths
+
+print(addition(3,4))
+print(subtraction(5,4))
+
+from package.subpackage.mult import *
+print(multiply(3,4))
